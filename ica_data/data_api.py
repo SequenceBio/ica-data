89f49719305d0e94f1597ba84ed917e64499d4cd
Jason <jasonmw_nf@hotmail.com>
2022-08-29 14:13:43 -0230

    trying to invoke find method

--- ica_data/data_api.py
+++ ica_data/data_api.py
@@ -87,7 +87,7 @@ class DataApi:
             filename = os.path.basename(file_path)
             results = self.api_client.get_project_data_list(project_id=self.project_id, file_path=[path], filename=[filename], filename_match_mode="EXACT", type="FILE")
             # file_id = results.items[0].data.id
-            file_id = find(file_path)
+            file_id = self.find(file_path)
 
             # Download file
             download = self.api_client.create_download_url_for_data(project_id=self.project_id, data_id=file_id)
